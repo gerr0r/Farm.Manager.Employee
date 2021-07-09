9677b40891958be7348eb2bd7780d943d523a8e3
Gergan Ruschev <gergan.ruschev@yara.com>
2021-07-09 15:25:02 +0300

    preliminary set up access for user account to read farm employees information

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,29 +1,38 @@
-const db = require('../db/models')
-const { hasRights } = require('../helpers/auth')
+const db = require("../db/models");
+const { hasRights } = require("../helpers/auth");
 
 module.exports = {
-    Query: {
-        async getEmployees(parent, args, context) {
-            const { token } = context
-            const adminId = hasRights(token, 'admin')
-            if (!adminId) throw new Error('Unauthorized')
-            return await db.Employee.findAll({where: { employerId: adminId }})
-        },
-        async getFarmEmployees(parent, args, context) {
-            const { token } = context
-            const adminId = hasRights(token, 'admin')
-            if (!adminId) throw new Error('Unauthorized')
-
-            return await db.Employee.findAll({where: { employerId: adminId, farmId: args.farmId }})
-        }
+  Query: {
+    async getEmployees(parent, args, context) {
+      const { token } = context;
+      const adminId = hasRights(token, "admin");
+      if (!adminId) throw new Error("Unauthorized");
+      return await db.Employee.findAll({ where: { employerId: adminId } });
     },
+    async getFarmEmployees(parent, args, context) {
+      const { token } = context;
 
-    Employee: {
-        async __resolveReference(employee) {
-            return await db.Employee.findByPk(employee.id)
-        },
-        farm(userFarm) {
-            return { __typename: "Farm", id: userFarm.farmId };
-        },
-    }
-}
+      // HACK: find a way to check user access for given farm
+      if (hasRights(token, "user")) {
+        return await db.Employee.findAll({
+            where: { farmId: args.farmId },
+          });
+      }
+      const adminId = hasRights(token, "admin");
+      if (!adminId) throw new Error("Unauthorized");
+
+      return await db.Employee.findAll({
+        where: { employerId: adminId, farmId: args.farmId },
+      });
+    },
+  },
+
+  Employee: {
+    async __resolveReference(employee) {
+      return await db.Employee.findByPk(employee.id);
+    },
+    farm(userFarm) {
+      return { __typename: "Farm", id: userFarm.farmId };
+    },
+  },
+};
