ad507f47efb91274851cd9c5bd1af6f38bcd994d
Gergan Ruschev <gergan.ruschev@yara.com>
2021-07-09 09:11:32 +0300

    feat/added Farm type as external reference

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -5,14 +5,25 @@ module.exports = {
     Query: {
         async getEmployees(parent, args, context) {
             const { token } = context
-            if (!hasRights(token, 'master')) throw new Error('Unauthorized')
-            return await db.Employee.findAll()
+            const adminId = hasRights(token, 'admin')
+            if (!adminId) throw new Error('Unauthorized')
+            return await db.Employee.findAll({where: { employerId: adminId }})
+        },
+        async getFarmEmployees(parent, args, context) {
+            const { token } = context
+            const adminId = hasRights(token, 'admin')
+            if (!adminId) throw new Error('Unauthorized')
+
+            return await db.Employee.findAll({where: { employerId: adminId, farmId: args.farmId }})
         }
     },
 
     Employee: {
         async __resolveReference(employee) {
             return await db.Employee.findByPk(employee.id)
-        }
+        },
+        farm(userFarm) {
+            return { __typename: "Farm", id: userFarm.farmId };
+        },
     }
 }
--- src/type-definitions/index.js
+++ src/type-definitions/index.js
@@ -3,10 +3,19 @@ const { gql } = require('apollo-server')
 module.exports = gql`
   extend type Query {
     getEmployees: [Employee]
+    getFarmEmployees(farmId: ID!): [Employee]
   }
 
   type Employee @key(fields: "id") {
     id: ID!
     name: String!
+    number: String
+    address: String
+    salary: Int
+    farm: Farm
+  }
+
+  extend type Farm @key(fields: "id") {
+    id: ID! @external
   }
 `
